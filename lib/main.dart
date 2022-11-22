e9d08c45e0d6d3ce4a8cccd936ee40beb14a7a04
carlos <carlos.barahona@ieselcalamot.com>
2022-11-22 18:45:02 +0100

    Correcció examen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:examen_calendari/editar_esdeveniment.dart';
+import 'package:examen_calendari/esdeveniments_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'modelo.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,95 +19,20 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(
-        title: "My home page",
-      ),
+      routes: {
+        '/': (context) => MyHomePage(
+              title: "My home page",
+            ),
+        '/esdeveniment/nou': (context) => EditarEsdevenimentForm(),
+      },
     );
   }
-}
-
-class Esdeveniment {
-  final DateTime horaInici, horaFinal;
-  final String titol;
-  final String? descripcio;
-
-  Esdeveniment(
-      {required this.horaInici,
-      required this.horaFinal,
-      required this.titol,
-      this.descripcio});
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({super.key, required this.title});
 
-  final esdeveniments = <Esdeveniment>[
-    Esdeveniment(
-        horaInici: DateTime(2022, 11, 1),
-        horaFinal: DateTime(2022, 11, 1, 23, 59),
-        titol: "Festiu",
-        descripcio: "Tots els Sants"),
-    Esdeveniment(
-        horaInici: DateTime(2022, 12, 6),
-        horaFinal: DateTime(2022, 12, 6, 23, 59),
-        titol: "Festiu",
-        descripcio: "Dia de la Constitució"),
-    Esdeveniment(
-        horaInici: DateTime(2022, 12, 8),
-        horaFinal: DateTime(2022, 12, 7, 23, 59),
-        titol: "Festiu",
-        descripcio: "Inmaculada Concepció"),
-    Esdeveniment(
-        horaInici: DateTime(2022, 12, 9),
-        horaFinal: DateTime(2022, 12, 8, 23, 59),
-        titol: "Festiu",
-        descripcio: "2n día lliure disposició"),
-    Esdeveniment(
-        horaInici: DateTime(2022, 11, 22, 17, 0),
-        horaFinal: DateTime(2022, 11, 22, 19, 20),
-        titol: "Examen",
-        descripcio: "Examen dispositius mòbils"),
-    Esdeveniment(
-        horaInici: DateTime(2022, 11, 22, 19, 20),
-        horaFinal: DateTime(2022, 11, 22, 21, 20),
-        titol: "Entrega",
-        descripcio: "Entrega projecte inicial"),
-    Esdeveniment(
-        horaInici: DateTime(2022, 11, 30, 16, 0),
-        horaFinal: DateTime(2022, 11, 30, 17, 0),
-        titol: "Entrega",
-        descripcio: "Pràctica programació serveis"),
-    Esdeveniment(
-        horaInici: DateTime(2022, 11, 30, 19, 20),
-        horaFinal: DateTime(2022, 11, 30, 20, 20),
-        titol: "Entrega",
-        descripcio: "Planificació inicial síntesi"),
-    Esdeveniment(
-        horaInici: DateTime(2022, 12, 12, 15, 0),
-        horaFinal: DateTime(2022, 12, 12, 16, 0),
-        titol: "Examen",
-        descripcio: "Test sistemes gestió"),
-    Esdeveniment(
-        horaInici: DateTime(2022, 12, 12, 18, 20),
-        horaFinal: DateTime(2022, 12, 12, 19, 20),
-        titol: "Entrega",
-        descripcio: "Exercicis dispositius mòbils"),
-    Esdeveniment(
-        horaInici: DateTime(2022, 11, 6, 13, 0),
-        horaFinal: DateTime(2022, 11, 6, 19, 0),
-        titol: "Dinar familiar",
-        descripcio: "Aniversari tia Julieta. Comprar regal!!"),
-    Esdeveniment(
-        horaInici: DateTime(2022, 11, 12, 22, 0),
-        horaFinal: DateTime(2022, 11, 13, 2, 0),
-        titol: "Concert",
-        descripcio: "Músics al carrer de Gavà"),
-    Esdeveniment(
-        horaInici: DateTime(2022, 11, 19, 10, 0),
-        horaFinal: DateTime(2022, 11, 19, 19, 0),
-        titol: "Hackaton",
-        descripcio: "Reptes ODS 2022"),
-  ];
+  final EsdevenimentsController controlador = EsdevenimentsController();
 
   final String title;
 
@@ -113,18 +41,55 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
+  DateTime _selectedDay=DateTime.now();
+  DateTime _focusedDay=DateTime.now();
+  CalendarFormat _calendarFormat=CalendarFormat.month;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body:
-          LlistatEsdevenimentsWidget(llistaEsdeveniments: widget.esdeveniments),
+      body: Column(
+        children: [
+          TableCalendar(
+            firstDay: DateTime.utc(2010, 10, 16),
+            lastDay: DateTime.utc(2030, 3, 14),
+            focusedDay: DateTime.now(),
+            selectedDayPredicate: (day) {
+              return isSameDay(_selectedDay, day);
+            },
+            onDaySelected: (selectedDay, focusedDay) {
+              setState(() {
+                _selectedDay = selectedDay;
+                _focusedDay = focusedDay; // update `_focusedDay` here as well
+              });
+            },
+            calendarFormat: _calendarFormat,
+            onFormatChanged: (format) {
+              setState(() {
+                _calendarFormat = format;
+              });
+            },
+            onPageChanged: (focusedDay) {
+              _focusedDay = focusedDay;
+            },
+            eventLoader: (day) {
+              return widget.controlador.getEsdeveniments(elDia: day);
+            },
+          ),
+          Expanded(
+            child: LlistatEsdevenimentsWidget(
+                llistaEsdeveniments: widget.controlador.getEsdeveniments(elDia: _selectedDay)),
+          ),
+        ],
+      ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          ScaffoldMessenger.of(context)
-              .showSnackBar(const SnackBar(content: Text("No implementat.")));
+        onPressed: () async {
+          await Navigator.of(context).pushNamed("/esdeveniment/nou");
+          setState(() {});
         },
         tooltip: 'Afegir esdeveniment',
         child: const Icon(Icons.add),
